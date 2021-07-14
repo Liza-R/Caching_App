fb35b95d35ad3defc4a40b43471e0f02a214d441
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-07-14 16:12:11 +0300

    Adding code for Weather (without caching and loading info for table & collection)

--- HW_9/Weather_Cache/Links.swift
+++ HW_9/Weather_Cache/Links.swift
@@ -8,20 +8,11 @@
 import Foundation
  
 var url_icon_upload = "http://openweathermap.org/img/wn/PICTURENAME@2x.png",
-    
-    url_today_uploadBase = "https://api.openweathermap.org/data/2.5/weather?q=\(cityNameBase)&appid=f786f3131537f8ee067b397b6f7753be",
-    url_fiveDays_uploadBase = "https://api.openweathermap.org/data/2.5/forecast?q=\(cityNameBase)&appid=f786f3131537f8ee067b397b6f7753be",
     url_today_uploadAlam = "https://api.openweathermap.org/data/2.5/weather?q=\(cityNameAlam)&appid=f786f3131537f8ee067b397b6f7753be",
     url_fiveDays_uploadAlam = "https://api.openweathermap.org/data/2.5/forecast?q=\(cityNameAlam)&appid=f786f3131537f8ee067b397b6f7753be"
     
 
 class changeURL{
-    func changeTodayURLBase(cityName: String){
-        url_today_uploadBase = "https://api.openweathermap.org/data/2.5/weather?q=\(cityNameBase)&appid=f786f3131537f8ee067b397b6f7753be"
-    }
-    func changeFiveDaysURLBase(cityName: String){
-        url_fiveDays_uploadBase = "https://api.openweathermap.org/data/2.5/forecast?q=\(cityNameBase)&appid=f786f3131537f8ee067b397b6f7753be"
-    }
     
     func changeTodayURLAlam(cityName: String){
         url_today_uploadAlam = "https://api.openweathermap.org/data/2.5/weather?q=\(cityNameAlam)&appid=f786f3131537f8ee067b397b6f7753be"
---
+++ HW_9/Weather_Cache/WeatherCollectionViewCellAlam.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCollectionViewCellAlam.swift
+//  HW_9
+//
+//  Created by Elizaveta Rogozhina on 14.07.2021.
+//
+
+import UIKit
+
+class WeatherCollectionViewCellAlam: UICollectionViewCell {
+    @IBOutlet weak var timeLabelAlam: UILabel!
+    @IBOutlet weak var temperLabelAlam: UILabel!
+    @IBOutlet weak var iconImageAlam: UIImageView!
+    @IBOutlet weak var descriptLabelAlam: UILabel!
+}
