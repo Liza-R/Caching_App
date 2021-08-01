b7bb2736741175e64f0fc4a18600957f59ba75de
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-08-01 15:27:59 +0300

    UserDefaults is ready!

---
+++ HW_9/UserDefaults/UserDefaults.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefaults.swift
+//  HW_9
+//
+//  Created by Elizaveta Rogozhina on 01.08.2021.
+//
+
+import Foundation
+
+class UserDef{
+    static let safekeepingUD = UserDef()
+    private let keyUserName = "UserDef.keyUserName",
+                keyUserLastName = "UserDef.keyUserLastName"
+    
+    var userName: String?{
+        set { UserDefaults.standard.set(newValue, forKey: keyUserName) }
+        get { return UserDefaults.standard.string(forKey: keyUserName)}
+    }
+    var userLastName: String?{
+        set { UserDefaults.standard.set(newValue, forKey: keyUserLastName) }
+        get { return UserDefaults.standard.string(forKey: keyUserLastName)}
+    }
+}
--- HW_9/UserDefaults/ViewController.swift
+++ HW_9/UserDefaults/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTF.text = UserDef.safekeepingUD.userName
+        lastNameTF.text = UserDef.safekeepingUD.userLastName
+        
+        self.nameTF.rx.text
+            .orEmpty
+            .subscribe(onNext: {new_Name in
+                UserDef.safekeepingUD.userName = new_Name
+        }).disposed(by: disposeBag)
+        
+        self.lastNameTF.rx.text
+            .orEmpty
+            .subscribe(onNext: {new_LastName in
+                UserDef.safekeepingUD.userLastName = new_LastName
+        }).disposed(by: disposeBag)
+        
+        print(UserDef.safekeepingUD.userName ?? "Name is Empty")
+        print(UserDef.safekeepingUD.userLastName ?? "Last Name is Empty")
     }
-
-
 }
 
