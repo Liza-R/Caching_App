c44fb0d327b3624a871cd3ac4396e6ae9e702297
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-08-01 19:06:05 +0300

    Add some code for RealmVC

--- HW_9/ToDo_Realm/RealmFile.swift
+++ HW_9/ToDo_Realm/RealmFile.swift
@@ -8,11 +8,31 @@
 import Foundation
 import RealmSwift
 
+class ToDoInfo: Object{
+    @objc dynamic var taskName = "",
+                      check = false,
+                      delite = false,
+                      qtTasks = 0
+}
+
 class RealmSaving{
     static let safekeepingRealm = RealmSaving()
+    private let realm = try! Realm()
     
     func safekeeping(){
+        let task = ToDoInfo()
+        task.taskName = taskDescript
+        task.check = checking
+        task.delite = deliting
+        task.qtTasks = qtTasks
+        try! realm.write{
+            realm.add(task)
+        }
+        
+        let allTasks = realm.objects(ToDoInfo.self)
+        for task in allTasks{
+            print(task)
+        }
         
     }
-    
 }
--- HW_9/ToDo_Realm/TDRealmViewController.swift
+++ HW_9/ToDo_Realm/TDRealmViewController.swift
@@ -7,17 +7,41 @@
 
 import UIKit
 
+var checking = false,
+    deliting = false,
+    taskDescript = "",
+    qtTasks = 0
+
 class TDRealmViewController: UIViewController {
 
     @IBOutlet weak var todoTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        RealmSaving.safekeepingRealm.safekeeping()
+        self.todoTable.rowHeight = 100
+        self.todoTable.dataSource = self
+        self.todoTable.reloadData()
     }
     
     @IBAction func addingButton(_ sender: Any) {
-        
+        qtTasks += 1
+        self.todoTable.reloadData()
     }
     
+}
+
+extension TDRealmViewController: UITableViewDataSource{
+
+    func tableView(_ tableView_Alam: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return qtTasks
+    }
+
+    func tableView(_ tableView_Alam: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell_Alam = tableView_Alam.dequeueReusableCell(withIdentifier: "realmCell", for: indexPath) as! RealmToDoTableViewCell
+        
+        
+        return cell_Alam
+    }
 }
