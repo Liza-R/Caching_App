cd8181fda4a17eb54c5b22fa7c7f404e27c849c6
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-08-04 19:56:17 +0300

    Adding swipes (checking & removing) to table (RealmVC) (don't function)

---
+++ HW_9/ToDo_Realm/CellActions.swift
@@ -0,0 +1,15 @@
+//
+//  CellActions.swift
+//  HW_9
+//
+//  Created by Elizaveta Rogozhina on 04.08.2021.
+//
+
+import Foundation
+
+class CellActions{
+    let actions = CellActions()
+    func removingCell(indPath: IndexPath){
+        
+    }
+}
--- HW_9/ToDo_Realm/TDRealmViewController.swift
+++ HW_9/ToDo_Realm/TDRealmViewController.swift
@@ -19,6 +19,7 @@ class TDRealmViewController: UIViewController {
         super.viewDidLoad()
         RealmSaving.safekeepingRealm.forUpload()
         self.todoTable.dataSource = self
+        self.todoTable.delegate = self
         self.todoTable.reloadData()
         self.labelForQT.text = "\(qt) task(-s)"
     }
@@ -34,16 +35,51 @@ class TDRealmViewController: UIViewController {
     }
 }
 
-extension TDRealmViewController: UITableViewDataSource{
+extension TDRealmViewController: UITableViewDataSource, UITableViewDelegate{
 
-    func tableView(_ tableView_Alam: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return qt
     }
     
-    func tableView(_ tableView_Alam: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let swipeRemove = UIContextualAction(style: .normal, title: "Remove"){ (action, view, success) in
+            self.todoTable.performBatchUpdates({
+                //self.model.CellActions().actions.removingCell(indPath: indexPath)
+                self.todoTable.deleteRows(at: [indexPath], with: .automatic)
+            }, completion: nil)
+            print("Removing \(indexPath.row)")
+        }
+        swipeRemove.backgroundColor = #colorLiteral(red: 0.646001092, green: 0.05260277429, blue: 0, alpha: 1)
+
+        let swipes = UISwipeActionsConfiguration(actions: [swipeRemove])
+        swipes.performsFirstActionWithFullSwipe = true
         
-        let cell_Alam = tableView_Alam.dequeueReusableCell(withIdentifier: "realmCell", for: indexPath) as! RealmToDoTableViewCell
+        return swipes
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?{
+        let swipeCheck = UIContextualAction(style: .normal, title: "Check"){ (action, view, success) in
+            
+            self.todoTable.performBatchUpdates({
+                //self.model.CellActions().actions.removingCell(indPath: indexPath)
+                self.todoTable.deleteRows(at: [indexPath], with: .automatic)
+            }, completion: nil)
+            print("Checking \(indexPath.row)")
+        }
+        swipeCheck.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+     
+        let swipes = UISwipeActionsConfiguration(actions: [swipeCheck])
+        swipes.performsFirstActionWithFullSwipe = true
+        
+        return swipes
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell_Alam = tableView.dequeueReusableCell(withIdentifier: "realmCell", for: indexPath) as! RealmToDoTableViewCell
 
         return cell_Alam
     }
+    
+    
 }
