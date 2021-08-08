57ec5d8e2767234516304dad32d9fba5de26caf0
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-08-08 18:34:48 +0300

    Transferring code from RealmVC to CoreDataVC

--- HW_9/ToDo_CoreData/CoreDataTableViewCell.swift
+++ HW_9/ToDo_CoreData/CoreDataTableViewCell.swift
@@ -10,13 +10,30 @@ import UIKit
 class CoreDataTableViewCell: UITableViewCell {
     @IBOutlet weak var taskTF: UITextField!
     @IBOutlet weak var tralingTaskTF: NSLayoutConstraint!
+    @IBOutlet weak var arrowBn: UIButton!
     
     @IBAction func didBeginTF(_ sender: Any) {
+        UIView.animate(withDuration: 0.33, delay: 0, options: .autoreverse, animations: {
+            self.tralingTaskTF.constant = 40
+        })
+        self.arrowBn.tintColor = .label
     }
     @IBAction func didEndTF(_ sender: Any) {
+        /*let stop = taskTF.attributedText?.string,
+            item = model[eventTF.tag]
+
+        try! realm.write({
+            item.taskNote = stop ?? ""
+        })
+        */
+        UIView.animate(withDuration: 0.33, delay: 0, options: .autoreverse, animations: {
+            self.tralingTaskTF.constant = 0
+        })
+        self.arrowBn.tintColor = .clear
     }
     
     @IBAction func arrowButton(_ sender: Any) {
+        didEndTF(true)
     }
     
     override func awakeFromNib() {
